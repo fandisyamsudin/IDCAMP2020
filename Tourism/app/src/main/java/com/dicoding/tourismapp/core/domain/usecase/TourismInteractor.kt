342dd51546fcc4d6360bc734095df9f1a67b6ff3
fandisyamsudin <fandisyamsudin20@gmail.com>
2021-01-02 21:32:08 +0700

    add RxJava2

--- Tourism/app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
+++ Tourism/app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -5,13 +5,14 @@ import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+import io.reactivex.Flowable
 
 class TourismInteractor(private val tourismRepository: ITourismRepository): TourismUseCase {
-    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> {
+    override fun getAllTourism(): Flowable<Resource<List<Tourism>>> {
         return tourismRepository.getAllTourism()
     }
 
-    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
+    override fun getFavoriteTourism(): Flowable<List<Tourism>> {
         return tourismRepository.getFavoriteTourism()
     }
 
