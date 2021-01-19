1695aa70a488c095dbfbe0cca022776a0a3e0252
fandisyamsudin <fandisyamsudin20@gmail.com>
2021-01-19 22:43:55 +0700

    Edit item_movie

--- MADE/app/src/main/java/com/idcamp2020/made/di/AppModule.kt
+++ MADE/app/src/main/java/com/idcamp2020/made/di/AppModule.kt
@@ -5,8 +5,6 @@ import com.idcamp2020.made.core.domain.usecase.MovieUseCase
 import com.idcamp2020.made.ui.detail.DetailViewModel
 import com.idcamp2020.made.ui.home.HomeViewModel
 import com.idcamp2020.made.ui.home.SearchViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
